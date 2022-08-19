c6b7599e7f4760d2693afc190194c77afec530ad
Ashley Dawson <ashley.dawson@deskpro.com>
2022-08-19 15:53:58 +0100

    Feedback fixes

--- src/components/Error/ErrorBlock.tsx
+++ src/components/Error/ErrorBlock.tsx
@@ -3,6 +3,7 @@ import "./ErrorBlock.css";
 import { Stack, useDeskproAppTheme } from "@deskpro/app-sdk";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { capitalize } from "lodash";
 
 export interface ErrorBlockProps {
   text: string|string[];
@@ -13,10 +14,10 @@ export const ErrorBlock: FC<ErrorBlockProps> = ({ text }: ErrorBlockProps) => {
 
   return (
     <Stack className="error-block" style={{ backgroundColor: theme.colors.red100 }}>
-      <FontAwesomeIcon icon={faExclamation} style={{ marginRight: "6px" }} />
+      <FontAwesomeIcon icon={faExclamation} style={{ marginRight: "6px", paddingTop: "2px" }} />
       <div className="error-block-messages">
         {Array.isArray(text) ? text.map((msg, idx) => (
-          <div className="error-block-message" key={idx}>{msg}</div>
+          <div className="error-block-message" key={idx}>{capitalize(msg)}</div>
         )) : text}
       </div>
     </Stack>
