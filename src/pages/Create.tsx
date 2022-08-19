a5b941ccab5e64506e7c83475c03adeb5275ff6c
Ashley Dawson <ashley.dawson@deskpro.com>
2022-08-19 11:18:23 +0100

    Refactored create process so that apollo errors are ignored

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -35,14 +35,27 @@ export const Create: FC = () => {
 
     setLoading(true);
 
-    createStory(client, data)
-      .then(async (res) => {
+
+    (async () => {
+        let res;
+
+        try {
+            res = await createStory(client, data);
+        } catch (e) {
+            console.error(e);
+        }
+
+        if (!res || !res?.id) {
+            console.error("Failed to create Shortcut story");
+            return;
+        }
+
         const {
-          groups,
-          epics,
-          workflows,
-          iterations,
-          projects,
+            groups,
+            epics,
+            workflows,
+            iterations,
+            projects,
         } = await getStoryDependencies(client);
 
         const states = (workflows ?? []).reduce((all: any[], workflow: any) => [...all, ...workflow.states], []);
@@ -57,60 +70,41 @@ export const Create: FC = () => {
         const workflow = (workflows ?? []).filter((w: { states: { id: number }[] }) => find(w.states, { id: stateId }))[0] ?? null;
 
         const metadata: ShortcutStoryAssociationProps = {
-          id: `${res.id}`,
-          name: res.name,
-          type: res.story_type,
-          projectId: res?.project_id ? `${res.project_id}` : undefined,
-          projectName: project?.name,
-          workflowId: `${workflow.id}`,
-          workflowName: workflow.name,
-          statusId: `${stateId}`,
-          statusName: state.name,
-          teamId: group?.id ? `${group.id}` : undefined,
-          teamName: group?.name,
-          iterationId: iteration?.id ? `${iteration.id}` : undefined,
-          iterationName: iteration?.name,
-          epicId: epic?.id ? `${epic.id}` : undefined,
-          epicName: epic?.name,
-          labels: ((res?.labels ?? []) as { id: number, name: string }[]).map<ShortcutStoryAssociationPropsLabel>((label) => ({
-            id: `${label.id}`,
-            name: label.name,
-          })),
+            id: `${res.id}`,
+            name: res.name,
+            type: res.story_type,
+            projectId: res?.project_id ? `${res.project_id}` : undefined,
+            projectName: project?.name,
+            workflowId: `${workflow.id}`,
+            workflowName: workflow.name,
+            statusId: `${stateId}`,
+            statusName: state.name,
+            teamId: group?.id ? `${group.id}` : undefined,
+            teamName: group?.name,
+            iterationId: iteration?.id ? `${iteration.id}` : undefined,
+            iterationName: iteration?.name,
+            epicId: epic?.id ? `${epic.id}` : undefined,
+            epicName: epic?.name,
+            labels: ((res?.labels ?? []) as { id: number, name: string }[]).map<ShortcutStoryAssociationPropsLabel>((label) => ({
+                id: `${label.id}`,
+                name: label.name,
+            })),
         };
 
         await client
-          .getEntityAssociation("linkedShortcutStories", ticketId)
-          .set<ShortcutStoryAssociationProps>(`${res.id}`, metadata)
+            .getEntityAssociation("linkedShortcutStories", ticketId)
+            .set<ShortcutStoryAssociationProps>(`${res.id}`, metadata)
         ;
 
-        console.log("HERE1");
+        const id = res.id;
 
-        return res.id;
-      })
-      .then((id: number) => {
-
-          console.log("HERE2", id, permalinkUrl);
-
-          return addExternalUrlToStory(client, `${id}`, permalinkUrl);
-      })
-      .then(() => {
-
-          console.log("HERE3");
-
-          return loadLinkedStories();
-      })
-      .then(() => {
-          console.log("HERE4");
+        await addExternalUrlToStory(client, `${id}`, permalinkUrl);
+        await loadLinkedStories();
 
         setLoading(false);
+
         dispatch({ type: "changePage", page: "home" });
-      })
-      .finally(() => {
-          console.log("HERE5");
-
-        setLoading(false);
-      })
-    ;
+    })();
   };
 
   return (
