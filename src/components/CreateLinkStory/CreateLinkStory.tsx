493e9797a4b54942c3a62ff37f4e8b7644ebab96
OJ Abba <phavorabba@gmail.com>
2025-04-03 15:02:19 +0100

    [Bug] Fix tab component not resizing gracefully (SC-180856)

--- src/components/CreateLinkStory/CreateLinkStory.tsx
+++ src/components/CreateLinkStory/CreateLinkStory.tsx
@@ -1,7 +1,5 @@
 import { FC } from "react";
-import "./CreateLinkStory.css";
-import { Button, Stack } from "@deskpro/deskpro-ui";
-import { useDeskproAppTheme } from "@deskpro/app-sdk";
+import { TwoButtonGroup } from "@deskpro/app-sdk";
 import { faSearch, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
@@ -12,34 +10,18 @@ export interface CreateLinkStoryProps {
 export const CreateLinkStory: FC<CreateLinkStoryProps> = ({
   selected,
 }: CreateLinkStoryProps) => {
-  const {
-    theme: { colors },
-  } = useDeskproAppTheme();
   const navigate = useNavigate();
 
+
   return (
-    <Stack
-      className="create-link"
-      justify="space-between"
-      align="center"
-      style={{ backgroundColor: colors.grey10 }}
-    >
-      <Button
-        text="Find Story"
-        intent="secondary"
-        icon={faSearch}
-        size="large"
-        className={`create-link-link ${selected === "create" && "unselected"}`}
-        onClick={() => navigate("/link")}
-      />
-      <Button
-        text="Create Story"
-        intent="secondary"
-        icon={faPlus}
-        size="large"
-        className={`create-link-create ${selected === "link" && "unselected"}`}
-        onClick={() => navigate("/create")}
-      />
-    </Stack>
-  );
+    <TwoButtonGroup
+      selected={selected === "link" ? "one" : "two"}
+      oneLabel="Find Story"
+      twoLabel="Create Story"
+      oneIcon={faSearch}
+      twoIcon={faPlus}
+      oneOnClick={() => navigate("/link")}
+      twoOnClick={() => navigate("/create")}
+    />
+  )
 };
