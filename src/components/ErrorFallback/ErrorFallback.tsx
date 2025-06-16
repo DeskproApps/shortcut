dbd4aadbdcc3ca8976b3409dd3129a578c4a6a20
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-16 17:05:13 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#63)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,20 +1,18 @@
 import { Stack, H1, H2, Button } from "@deskpro/deskpro-ui";
 import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { FallbackRender } from "@sentry/react";
 
-export const ErrorFallback = ({
+export const ErrorFallback: FallbackRender = ({
   error,
-  resetErrorBoundary = () => {},
-}: {
-  error: Error;
-  resetErrorBoundary?: () => void;
+  resetError,
 }) => {
   return (
     <Stack vertical gap={10} role="alert">
       <H1>Something went wrong:</H1>
-      <H2>{error.message}</H2>
+      <H2>{(error as Error|undefined)?.message}</H2>
       <Button
         text="Reload"
-        onClick={resetErrorBoundary}
+        onClick={resetError}
         icon={faRefresh}
         intent="secondary"
       />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import './instrument';
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { query } from "./utils/query";
 import { ReplyBoxProvider } from "./hooks";
@@ -16,13 +16,13 @@ import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "./main.css";
 import "simplebar/dist/simplebar.min.css";
 import { Scrollbar } from "@deskpro/deskpro-ui";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
 TimeAgo.addDefaultLocale(en);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render(
   <StrictMode>
     <Scrollbar style={{ height: "100%", width: "100%" }}>
@@ -31,7 +31,7 @@ root.render(
           <QueryClientProvider client={query}>
             <StoreProvider>
               <Suspense fallback={<LoadingSpinner />}>
-                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <ErrorBoundary fallback={ErrorFallback}>
                   <ReplyBoxProvider>
                     <App />
                   </ReplyBoxProvider>
